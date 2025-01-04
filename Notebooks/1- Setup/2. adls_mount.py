9ec26bf8ff7893dbe5a97a629fbbda296171ccd7
Niloofar Kandi <kandi.niloofar@outlook.com>
2025-01-04 11:09:21 +1100

    Update 2. adls_mount.py

--- Notebooks/1- Setup/2. adls_mount.py
+++ Notebooks/1- Setup/2. adls_mount.py
@@ -1,6 +1,6 @@
 # Databricks notebook source
 storageAccountName = "ttadlsdev"
-storageAccountAccessKey = dbutils.secrets.get('tt-hc-kv', 'tt-adls-access-key-dev')
+storageAccountAccessKey = dbutils.secrets.get('kv-healthcare-de-project', 'tt-adls-access-key-dev')
 mountPoints=["gold","silver","bronze","landing","configs"]
 for mountPoint in mountPoints:
     if not any(mount.mountPoint == f"/mnt/{mountPoint}" for mount in dbutils.fs.mounts()):
